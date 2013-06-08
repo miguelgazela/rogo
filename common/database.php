a4c2ca449c72174527236b3cd0a5f2717af0e5e4
Miguel Oliveira <miguel.gazela@gmail.com>
2013-06-08 17:28:51 +0100

    ups

--- common/database.php
+++ common/database.php
@@ -1,6 +1,6 @@
 <?php
     // Connect to the Database
-    $db = new PDO('pgsql:host=vdbm;dbname=lbaw12201', 'lbaw12201', 'mU339ft');
+    $db = new PDO('pgsql:host=vdbm;dbname=lbaw12201', 'username', 'password');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     //$db->exec("SET search_path TO proto"); // the schema is in public, no need for this
